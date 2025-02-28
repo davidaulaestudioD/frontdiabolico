440b2c2d92aab778c777ff9046073cdd359e1de4
SamuelDesN <vazquezsamuel877@gmail.com>
2025-02-28 16:19:23 +0100

    test32

--- funciones.js
+++ funciones.js
@@ -1,14 +1,45 @@
 $(document).ready(function () {
-  
-  $(".image-pedidos").click(function (e) {
+  $(".boton-login").click(function(e){
+    e.preventDefault();
+    
+    var usuario = $("#usuario").val().trim();
+    var contraseña = $("#contraseña").val().trim();
+    
+    
+    // Enviamos los datos al backend usando $.ajax (método GET en este ejemplo)
+    $.ajax({
+      url: "http://localhost:3000/api/login",
+      method: "GET", 
+      data: { usuario: usuario, contraseña: contraseña },
+      dataType: "json",
+      success: function(response) {
+          console.log(response);
+          
+
+        if(response.message && response.message.indexOf("Login exitoso") !== -1){
+          location.replace("home.html");
+        } else {
+          location.reload();
+        }
+      },
+      error: function(xhr) {
+        if (xhr.responseJSON && xhr.responseJSON.error) {
+          alert( xhr.responseJSON.error);
+        } else {
+          alert("Error al iniciar sesión");
+        }
+      }
+    });
+  });
+  $(".image-pedidos").click(function(e){
     location.replace("pedidos.html");
   });
 
-  $(".image-mesas").click(function (e) {
+  $(".image-mesas").click(function(e){
     location.replace("mesas.html");
   });
 
-  $(".logo-name").click(function (e) {
+  $(".logo-name").click(function(e){
     location.replace("home.html");
   });
   $(".boton-registro").click(function (e) {
@@ -63,52 +94,6 @@ $(document).ready(function () {
   });
   $(".logout").on("click", function () {
     location.replace("index.html");
-  });
-  
-  $(".boton-login").click(function(e){
-    e.preventDefault();
-    
-    var usuario = $("#usuario").val().trim();
-    var contraseña = $("#contraseña").val().trim();
-    
-    
-    // Enviamos los datos al backend usando $.ajax (método GET en este ejemplo)
-    $.ajax({
-      url: "http://localhost:3000/api/login",
-      method: "GET", 
-      data: { usuario: usuario, contraseña: contraseña },
-      dataType: "json",
-      success: function(response) {
-          console.log(response);
-          
-
-        if(response.message && response.message.indexOf("Login exitoso") !== -1){
-          location.replace("home.html");
-        } else {
-          location.reload();
-        }
-      },
-      error: function(xhr) {
-        if (xhr.responseJSON && xhr.responseJSON.error) {
-          alert( xhr.responseJSON.error);
-        } else {
-          alert("Error al iniciar sesión");
-        }
-      }
-    });
-  });
-
-
-  $(".image-pedidos").click(function(e){
-    location.replace("pedidos.html");
-  });
-
-  $(".image-mesas").click(function(e){
-    location.replace("mesas.html");
-  });
-
-  $(".logo-name").click(function(e){
-    location.replace("home.html");
   });
 
 })
