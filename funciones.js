7037735115e322f2f034bcceb1f35b66d34ba897
SamuelDesN <vazquezsamuel877@gmail.com>
2025-02-28 16:01:53 +0100

    modified:   funciones.js

--- funciones.js
+++ funciones.js
@@ -1,23 +1,58 @@
 $(document).ready(function(){
-    $(".boton-login").click(function(e){
-      e.preventDefault();
-      
-      var usuario = $("#usuario").val().trim();
-      var contraseña = $("#contraseña").val().trim();
-      
-      
+  $(".boton-login").click(function(e){
+    e.preventDefault();
+    
+    var usuario = $("#usuario").val().trim();
+    var contraseña = $("#contraseña").val().trim();
+    
+    
+    // Enviamos los datos al backend usando $.ajax (método GET en este ejemplo)
+    $.ajax({
+      url: "http://localhost:3000/api/login",
+      method: "GET", 
+      data: { usuario: usuario, contraseña: contraseña },
+      dataType: "json",
+      success: function(response) {
+          console.log(response);
+          
+
+        if(response.message && response.message.indexOf("Login exitoso") !== -1){
+          location.replace("home.html");
+        } else {
+          location.reload();
+
+        }
+      },
+      error: function(xhr) {
+        if (xhr.responseJSON && xhr.responseJSON.error) {
+          alert( xhr.responseJSON.error);
+        } else {
+          alert("Error al iniciar sesión");
+        }
+      }
+    });
+});
+  $(".boton-registro").click(function(e){
+    e.preventDefault();
+
+    var usuarioNuevo = $("#usuarioNuevo").val()
+    var contraseñaNueva = $("#contraseñaNueva").val()
+    var contraseñaNuevaConfirmar = $("#contraseñaNuevaConfirmada").val()
+  if(usuarioNuevo){
+    if(contraseñaNueva && contraseñaNueva==contraseñaNuevaConfirmar){
       $.ajax({
-        url: "http://localhost:3000/api/login",
-        method: "GET", 
-        data: { usuario: usuario, contraseña: contraseña },
+        url: "http://localhost:3000/api/registro",
+        type: "POST",
+        contentType: "application/json", 
+        data: JSON.stringify({ 
+          nombre: usuarioNuevo,
+          password: contraseñaNueva,
+          tipo:"normal"
+        }),
         dataType: "json",
         success: function(response) {
-
-          if(response.message && response.message.indexOf("Login exitoso") !== -1){
-            location.replace('home.html')
-          } else {
-           location.reload()
-          }
+            console.log(response);
+            location.replace("index.html")
         },
         error: function(xhr) {
           if (xhr.responseJSON && xhr.responseJSON.error) {
@@ -27,63 +62,30 @@ $(document).ready(function(){
           }
         }
       });
-    });
-    $(".boton-registro").click(function(e){
-      e.preventDefault();
-      
-      var usuarioNuevo = $("#usuarioNuevo").val()
-      var contraseñaNueva = $("#contraseñaNueva").val()
-      var contraseñaNuevaConfirmar = $("#contraseñaNuevaConfirmada").val()
-    if(usuarioNuevo){
-      if(contraseñaNueva && contraseñaNueva==contraseñaNuevaConfirmar){
-        $.ajax({
-          url: "http://localhost:3000/api/registro",
-          type: "POST",
-          contentType: "application/json", 
-          data: JSON.stringify({ 
-            nombre: usuarioNuevo,
-            password: contraseñaNueva,
-            tipo:"normal"
-          }),
-          dataType: "json",
-          success: function(response) {
-              console.log(response);
-              location.replace("index.html")
-          },
-          error: function(xhr) {
-            if (xhr.responseJSON && xhr.responseJSON.error) {
-              alert( xhr.responseJSON.error);
-            } else {
-              alert("Error al iniciar sesión");
-            }
-          }
-        });
-      }
-      else{
-        alert(!usuarioNuevo)
-        $("#usuarioNuevo").val("")
-        $("#contraseñaNuevaConfirmada").val("")
-        $("#contraseñaNueva").val("")
-  
-      }
     }
     else{
-      alert(!usuario)
+      alert(!usuarioNuevo)
       $("#usuarioNuevo").val("")
       $("#contraseñaNuevaConfirmada").val("")
       $("#contraseñaNueva").val("")
 
     }
-    });
+  }
+  else{
+    alert(!usuario)
+    $("#usuarioNuevo").val("")
+    $("#contraseñaNuevaConfirmada").val("")
+    $("#contraseñaNueva").val("")
 
-    $('.registrarme').on('click',function(){
-      location.replace('registro.html')
+  }
+  $('.registrarme').on('click',function(){
+    location.replace('registro.html')
+  })
+  $('.logear').on('click',function(){
+    location.replace('index.html')
+  })
+  $('.logout').on('click',function(){
+    location.replace("index.html")
+  })
     })
-    $('.logear').on('click',function(){
-      location.replace('index.html')
-    })
-    $('.logout').on('click',function(){
-      location.replace("index.html")
-    })
-});
-  
+})
